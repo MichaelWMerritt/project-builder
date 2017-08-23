cb444b3cf90eeb6c8d9abee9ced65521ee065b99
michaelwmerritt <pjjdmm@gmail.com>
2017-08-23 10:05:04 -0400

    Updating logic to work around a local issue with ant build (customer and ui dependencies)

--- builder/ant_builder.go
+++ builder/ant_builder.go
@@ -3,6 +3,7 @@ package builder
 import (
 	"github.com/michaelwmerritt/project-builder/model"
 	"os/exec"
+	"strings"
 )
 
 type AntBuilder struct{}
@@ -12,12 +13,12 @@ func NewAntBuilder() AntBuilder {
 }
 
 func (antBuilder AntBuilder) Build(release model.Release, modules []model.Module, path string) (err error) {
-	buildModuleMap := make(map[*model.Module]bool)
+	buildModuleMap := make(map[string]model.Module)
 	moduleDependencyMap := make(map[string]bool)
 
 	for _, module := range modules {
 		if module.BuildModule {
-			buildModuleMap[&module] = module.BuilderDependencies
+			buildModuleMap[module.Id] = module
 		} else {
 			if !module.BuildInfrastructure {
 				moduleDependencyMap[module.Id] = true
@@ -28,20 +29,33 @@ func (antBuilder AntBuilder) Build(release model.Release, modules []model.Module
 		}
 	}
 
-	for key, requiresBuildDependencies := range buildModuleMap {
-		dependencies := "-Dui.dependencies="
-		if requiresBuildDependencies {
-			dependencies += antBuilder.createDependencyString(moduleDependencyMap)
+	for _, module := range buildModuleMap {
+		customerDependencies := ""
+		uiDependencies := ""
+		if module.BuilderDependencies {
+			keys := make([]string, 0, len(moduleDependencyMap))
+			newKeys := make([]string, 0, len(moduleDependencyMap) - 1)
+			customerDependency := ""
+			for k := range moduleDependencyMap {
+				keys = append(keys, k)
+			}
+			for i := 0; i < len(keys); i++ {
+				if i == 0 {
+					customerDependency = keys[0]
+				} else {
+					newKeys = append(newKeys, keys[i])
+				}
+			}
+
+			customerDependencies = "-Dcustomer.dependencies=" + customerDependency
+			uiDependencies = antBuilder.createDependencyString("-Dui.dependencies=", newKeys)
 		}
-		cmd := exec.Command("ant", "-f", key.CreateBuildFilePath(path), dependencies)
+		cmd := exec.Command("ant", "-f", module.CreateBuildFilePath(path), customerDependencies, uiDependencies)
 		err = executeCommand(cmd)
 	}
 	return
 }
 
-func (antBuilder AntBuilder) createDependencyString(dependencyMap map[string]bool) (dependencies string) {
-	for key := range dependencyMap {
-		dependencies = dependencies + "," + key
-	}
-	return
+func (antBuilder AntBuilder) createDependencyString(tag string, keys []string) string {
+	 return tag + strings.Join(keys, ",")
 }
